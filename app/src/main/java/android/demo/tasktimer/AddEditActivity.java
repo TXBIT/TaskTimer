f63388fff2428f1d87767274c3936eb6a8aa8b62
thienhieu215 <s3681439@rmit.edu.vn>
2018-11-26 01:06:26 +0700

    Fix edit task inpotrait mode

--- app/src/main/java/android/demo/tasktimer/AddEditActivity.java
+++ app/src/main/java/android/demo/tasktimer/AddEditActivity.java
@@ -1,6 +1,8 @@
 package android.demo.tasktimer;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -18,6 +20,29 @@ public class AddEditActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        AddEditActivityFragment fragment = new AddEditActivityFragment();
+
+//        Bundle arguments = new Bundle();
+        Bundle arguments = getIntent().getExtras();
+        // add the task to a bundle
+        // and add the bundle to the fragments arguments
+//        arguments.putSerializable(Task.class.getSimpleName(), task);
+        fragment.setArguments(arguments);
+
+        // get a fragment manager then call beginTransaction method
+        // receive a fragment transaction object that is used to perform the wanted operations
+        // adding, removing, replacing fragments are done through a fragment transaction
+        // fragment transaction queues up all changes then performs them once commit is called
+        // by that way, everything seems smooth
+        // and users will not see gaps appearing when fragments removed and new ones added
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // the first parameter is the id for the FrameLayout task_details_container that we are going to put the fragment into
+// add => replace because of overlapping
+        fragmentTransaction.replace(R.id.fragment, fragment);
+        fragmentTransaction.commit();
     }
 
 }
